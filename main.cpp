2ecd5cfbce417ef4bd591fa20dabbfc9b53d1c30
vava94 <vitkiselyov@gmail.com>
2024-04-26 16:03:55 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by vok on 19.10.23.
+//
+#include <iostream>
+#include "ColorConverter/ColorConverter.h"
+#include "CameraHandler/CameraHandler.hpp"
+#include "CameraHandler/V4L2Handler.hpp"
+int main() {
+    V4L2Handler v4L2Handler = V4L2Handler();
+    return 0;
+} 
